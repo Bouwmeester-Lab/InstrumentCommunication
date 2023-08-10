758ddcf69edb9735b12d1fe87c7c3dc2f5460597
Henry <emorell96@hotmail.com>
2023-08-10 15:06:37 +0200

    fixed test

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def append_data(filename : str, data : Union[list[float], float], datetime : Uni
     It can add the datetime as the first column to the data.
     You can specify the format to use to display the saved data in the CSV file for the Data. Usually uses 3 decimal values scientific notation (Small e).
     """
-    with open(filename, 'a') as f:
+    with open(filename, 'a', newline='') as f:
         writer = csv.writer(f)
         try:
             values = map(lambda voltage : f"{voltage:^{data_format}}", data)
@@ -24,6 +24,6 @@ def append_data(filename : str, data : Union[list[float], float], datetime : Uni
         else:
             writer.writerow(values)
 
-append_data("test_date.csv", data=[1,2,3,4], datetime=datetime.utcnow())
-append_data("test_no_date.csv", data=[1,2,3,4,5], datetime=None)
-append_data("test_single_value.csv", data=2, datetime=datetime.utcnow())
+append_data("test/test_date.csv", data=[1,2,3,4], datetime=datetime.utcnow())
+append_data("test/test_no_date.csv", data=[1,2,3,4,5], datetime=None)
+append_data("test/test_single_value.csv", data=2, datetime=datetime.utcnow())
