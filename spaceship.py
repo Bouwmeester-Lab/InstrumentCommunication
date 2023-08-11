d4ce958d20a364c79dc563b3ff710dcac0749d0f
emorell96 <emorell96@hotmail.com>
2023-08-11 15:11:41 +0200

    added support for all the parameters of LaserManager in the spaceship.py code

--- spaceship.py
+++ spaceship.py
@@ -25,12 +25,22 @@ def main(scope_resource_name : str,
           peak_threshold : float = 1,
           min_coupling : float = 0.8,
           no_light_threshold : float = 0.01,
-          period_in_seconds : int = 120) -> None:
+          period_in_seconds : int = 120,
+          max_resonance_search_steps : int = 10,
+          sweeping_period : int = 10,
+          sweeping_total_range : float = 0.6,
+          sweeping_steps : int = 25) -> None:
     # ask the scheduler
     with ag.AgilentScope(scope_resource_name) as scope:
         with zi.ZurichInstruments(zi_device_serial_name, api_level) as zurich:
             laser = Laser(mode_hop_zones, zurich, laser_channel_zi)
-            laser_manager = LaserManager(zurich, scope, scope_channel, laser)
+            laser_manager = LaserManager(zurich, scope, scope_channel, laser, 
+                                         peak_threshold=peak_threshold,
+                                         min_coupling=min_coupling,
+                                         max_search_steps=max_resonance_search_steps,
+                                         sweep_iteration_period=sweeping_period,
+                                         sweep_total_range= sweeping_total_range,
+                                         sweep_steps=sweeping_steps)
 
             loop = task.LoopingCall(laser_manager.manage_loop, filename)
             loop.start(period_in_seconds)
@@ -41,6 +51,7 @@ def main(scope_resource_name : str,
 
 
 if __name__ == "__main__": # runs only if ran directly. This is not a library
+
     period_in_seconds = 20 # how often do you want to run the measurement code?
     scope_resource_name = "USB0::0x0957::0x179A::MY51450715::0::INSTR" # the scope VISA resource name.
     zi_device_serial_name = "dev812" # the name of the zurich instruments
@@ -48,12 +59,26 @@ if __name__ == "__main__": # runs only if ran directly. This is not a library
     
     file_name_path = "data/data" #this the prefix of all data files generated
 
+    peak_threshold = 0.15 # the minimum height of a peak to be considered a resonance
+    min_coupling = 0.5 # min coupling for considering a resonance as found
+    max_resonance_search_steps = 10 # how many search steps occur before giving up!
+    sweeping_period = 10 # changes how often a sweep occurs
+    sweeping_total_range = 0.6 # the total range of the sweep
+    sweeping_steps = 25 # total number of steps in the sweep
+
     mode_hop_zones = [(float("-inf"), 1.4), (5.4, float("inf"))] # there's a mode hop between the smallest voltage to 1.4V and another between 5.4V and the largest voltage.
 
+
+
     main(scope_resource_name,
-          zi_device_serial_name, 
-          api_level, file_name_path,
-          period_in_seconds = period_in_seconds,
-            mode_hop_zones=mode_hop_zones,
-             laser_channel_zi= 2,
-              scope_channel=1 )
+        zi_device_serial_name, 
+        api_level, file_name_path,
+        period_in_seconds = period_in_seconds,
+        mode_hop_zones=mode_hop_zones,
+        laser_channel_zi= 2,
+        scope_channel=1,
+        peak_threshold=peak_threshold,
+        min_coupling=min_coupling,
+        sweeping_period=sweeping_period,
+        sweeping_total_range=sweeping_total_range,
+        sweeping_steps = sweeping_steps)
